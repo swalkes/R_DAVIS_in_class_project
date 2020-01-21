0e589b0ba506ef2af8e978a9e0415f431f7abe5e
Smol Sam <Walkes@campus-018-018.ucdavis.edu>
2020-01-21 08:20:50 -0800

    help me

---
+++ Week2.R
@@ -0,0 +1,19 @@
+# how R talks back 
+log_of_word <- log("word")
+log_of_word
+
+# errors stop the whole process and do not give an output 
+
+log_of_negative <- log(-2)
+log_of_negative
+# code can run with warnings, but can lead to issues later in the script
+# messages are not anything to worry about
+
+# set working directory
+getwd()
+setwd("/Users/Walkes/Desktop/R_DAVIS_2020") #absolute filepath
+"R_DAVIS_in_class_project-master/data/" ## relative pathway, more useful if sharing data; press tab 
+## to see files in direct
+".." # move up a level
+
+
